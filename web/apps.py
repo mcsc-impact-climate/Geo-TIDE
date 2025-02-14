caf496f427d68240fe09aebc8e64cff62b76c59e
Danika MacDonell <danikam@mit.edu>
2025-02-13 18:48:24 -0800

    Fixes to complete rename to geo-tide

--- web/apps.py
+++ web/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GeoFTADSConfig(AppConfig):
+class GeoTIDEConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "geo_ftads"
+    name = "geo_tide"
